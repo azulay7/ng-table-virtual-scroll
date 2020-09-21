3ac4541241686bff6253fafda7b067682e25f411
azulay7@gmail.com <Amit.Azulay@orbotech.com>
2020-09-21 09:51:04 +0300

    add to fork

--- projects/ng-table-virtual-scroll/src/public-api.ts
+++ projects/ng-table-virtual-scroll/src/public-api.ts
@@ -2,7 +2,36 @@
  * Public API Surface of ng-fixed-size-table-virtual-scroll
  */
 
+import { BehaviorSubject, Subject } from 'rxjs';
+import { CdkVirtualScrollViewport, VirtualScrollStrategy } from '@angular/cdk/scrolling';
+import { ListRange } from '@angular/cdk/collections';
+import { TSVStrategyConfigs } from './lib/fixed-size-table-virtual-scroll-strategy';
+
+
 export * from './lib/table-virtual-scroll.module';
 export * from './lib/table-item-size.directive';
-export * from './lib/fixed-size-table-virtual-scroll-strategy';
+// export * from './lib/fixed-size-table-virtual-scroll-strategy';
 export * from './lib/table-data-source';
+
+export declare class FixedSizeTableVirtualScrollStrategy implements VirtualScrollStrategy {
+    private rowHeight;
+    private headerHeight;
+    private footerHeight;
+    private bufferMultiplier;
+    private indexChange;
+    stickyChange: Subject<number>;
+    viewport: CdkVirtualScrollViewport;
+    renderedRangeStream: BehaviorSubject<ListRange>;
+    scrolledIndexChange: import("rxjs").Observable<number>;
+    readonly dataLength: number;
+    private _dataLength;
+    attach(viewport: CdkVirtualScrollViewport): void;
+    detach(): void;
+    onContentScrolled(): void;
+    onDataLengthChanged(): void;
+    onContentRendered(): void;
+    onRenderedOffsetChanged(): void;
+    scrollToIndex(index: number, behavior?: ScrollBehavior): void;
+    setConfig(configs: TSVStrategyConfigs): void;
+    private updateContent;
+}
